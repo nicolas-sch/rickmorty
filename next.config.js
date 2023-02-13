7a88108f092277d4ca70a240ad2e19d6c5bb9d80
iRepo <iraitz.repository@gmail.com>
2023-02-13 09:00:11 +0100

    deploy new test structure

--- next.config.js
+++
@@ -1,5 +0,0 @@
-// next.config.js
-module.exports = {
-  excludeFile: (str) => /\*.{spec,test,ui-jest.spec,unit.spec}.[tj]s?(x)/.test(str),
-  pageExtensions: ["!.ui-jest.spec.ts(x)", "!.unit.spec.ts(x)"],
-};
